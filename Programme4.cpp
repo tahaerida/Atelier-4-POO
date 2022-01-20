9adbd3ff1831b47096c0e803951b29090ebe096a
TAHA ELAMINE RIDA <95881846+tahaerida@users.noreply.github.com>
2022-01-20 01:20:07 +0000

    Update Programme4.cpp

--- Programme4.cpp
+++ Programme4.cpp
@@ -1,56 +1,73 @@
 #include <iostream>
-#include <algorithm>
+#include <string>
 #include <list>
 using namespace std;
 
-class person{
-	public:
-	string firstName, lastName;
+class person {
+	string nom, prenom;
 	int age;
-	//for initialise the objects
-	void set(string firstName, string lastName, int age){
-		this->firstName = firstName;
-		this->lastName = lastName;
+public:
+	void set(string nom, string prenom, int age) {
+		this->nom = nom;
+		this->prenom = prenom;
 		this->age = age;
 	}
+	string get_nom() {
+		return nom;
+	}
+	string get_prenom() {
+		return prenom;
+	}
+	int get_age() {
+		return age;
+	}
+	void print() {
+		cout << "nom : " << nom << ", prenom : " << prenom << ", age : " << age << endl;
+	}
+	friend bool Compare_name(person, person);
 };
 
-int main(){
-	list<person> myList;//creating list
-	person myAlgoPerson;//creating object of the class
-	string fName, Lname;
-	int AGE;
-	cout<<"Entering information(add a rendom person with negative age for stop insering):\n"<<endl;
-	do{
-		cout<<"\nEnter first name please : ";
-		getline(cin, fName);
-		cout<<"Enter last name please : ";
-		getline(cin, Lname);
-		cout<<"Enter age please : ";
-		cin>>AGE;
-		getchar();
-		myAlgoPerson.set(fName, Lname, AGE);//initialise the objects
-		myList.push_back(myAlgoPerson);//adding objects to the list
-	}while(AGE>0);
-	myList.pop_back();//deleting the last rendom object
-	
-	cout<<"\n";
-	//Displaying the list
-	for(list<person>:: iterator itr = myList.begin(); itr!= myList.end() ; itr++){
-		int count = 1;
-		string firstname = itr->firstName;
-		string lastname = itr->lastName;
-		int aage = itr->age;
-		cout<<"Person"<<count<<"\n\tFirst name : "<<firstname<<"\n\tLast name : "<<lastname<<"\n\tAge : "<<aage<<endl;
-		count++;
-	}
-	return 0;
+bool Compare_name(person First, person Next)
+{
+	if (First.nom < Next.nom)
+		return true;
+	if (First.nom > Next.nom)
+		return false;
+	return true;
 }
 
+int main() {
+	list<person> mylist;
+	person man;
+	string nom, prenom;
+	int age = 0, i = 1;
+	cout << "Adding persons (enter random person with age = -999 to stop) :\n";
+	while (age != -999) {
+		cout << "Enter person" << i << " nom : ";
+		getline(cin, nom);
+		cout << "Enter person" << i << " prenom : ";
+		getline(cin, prenom);
+		cout << "Enter person" << i << " age : ";
+		cin >> age;
+		getchar();
 
+		if (age != -999) {
+			man.set(nom, prenom, age);
+			mylist.push_back(man);
+		}
+	}
+	
+	cout << "\n\n\n";
+	cout << "Before sorting :\n\n";
+	for (list<person>::iterator it = mylist.begin(); it != mylist.end(); it++) {
+		it->print();
+	}
+	cout << "\n\n\n";
+	cout << "After sorting :\n\n";
+	mylist.sort(Compare_name);
+	for (list<person>::iterator iter = mylist.begin(); iter != mylist.end(); iter++) {
+		iter->print();
+	}
 
-
-
-
-
-
+	return 0;
+}
